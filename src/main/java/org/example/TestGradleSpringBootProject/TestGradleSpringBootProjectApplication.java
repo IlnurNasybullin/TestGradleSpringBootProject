6c516f3f11e7a13ba7a9ec18d04d8c415a952671
IlnurNasybullin <ilnur.nasybullin2000@yandex.ru>
2022-04-06 09:54:49 +0300

    simple Spring Boot project

---
+++ src/main/java/org/example/TestGradleSpringBootProject/TestGradleSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.TestGradleSpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGradleSpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGradleSpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/TestGradleSpringBootProject/TestGradleSpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.TestGradleSpringBootProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGradleSpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
